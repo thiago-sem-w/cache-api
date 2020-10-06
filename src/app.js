004290cfc7843616a3647a0a4c8ea5635dc15e34
Thiago Sem W <thiagosemw@gmail.com>
2020-10-05 21:22:49 -0300

    add error handler

--- src/app.js
+++ src/app.js
@@ -14,4 +14,16 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 
+app.use('*', async (req, res, next) => {
+	res.status(404)
+	next(new Error('page not found'))
+})
+
+app.use(async (err, req, res, next) => {
+	const statusCode = res.statusCode || 500
+	res
+		.status(statusCode)
+		.json({ error: err.message })
+})
+
 module.exports = app
